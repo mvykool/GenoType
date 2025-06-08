6c8692cbefaee1015e518efef656f3b6b74a29e8
Maicol Hernandez <maykolandres4@gmail.com>
2025-06-08 05:48:02 -0400

    added types and created new get route

--- app-server/src/main.rs
+++ app-server/src/main.rs
@@ -1,13 +1,20 @@
-use axum::{routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Json, Router};
 use std::net::SocketAddr;
 use tower_http::cors::{Any, CorsLayer};
+
+// new types
+mod types;
+use types::Person;
 
 #[tokio::main]
 
 async fn main() {
     let cors = CorsLayer::new().allow_origin(Any);
 
-    let app = Router::new().route("/", get(root)).layer(cors);
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/people", get(get_people))
+        .layer(cors);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("Listening on {}", addr);
@@ -20,4 +27,27 @@ async fn main() {
 
 async fn root() -> &'static str {
     "Hello, world"
+}
+
+async fn get_people() -> impl IntoResponse {
+    let people = vec![
+        Person {
+            name: String::from("Person A"),
+            age: 36,
+            favorite_food: Some(String::from("Pizza")),
+        },
+        Person {
+            name: String::from("Person b"),
+            age: 10,
+            favorite_food: Some(String::from("coca-cola")),
+        },
+        Person {
+            name: String::from("Person C"),
+            age: 20,
+            favorite_food: None,
+        },
+
+    ];
+
+    (StatusCode::OK, Json(people))
 }
